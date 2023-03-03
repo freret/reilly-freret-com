d254f66e176e2c1c80572ecdb65215363e41fc15
Reilly Freret <freret@wustl.edu>
2023-03-03 17:57:32 -0500

    padding

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div style={{"max-width": "1000px", "margin": "auto"}}>
+    <div style={{"max-width": "1000px", "margin": "auto", "padding": "30px"}}>
       {children}
     </div>
   )
