2a7a478e2180d164264d7f8049e3d79d5eaab841
Reilly Freret <freret@wustl.edu>
2023-03-03 19:58:04 -0500

    using router links instead of anchor tags

--- src/404.js
+++ src/404.js
@@ -1,9 +1,11 @@
+import { Link } from "inferno-router";
+
 export default function PageNotFound() {
   return (
     <>
     <h1>404</h1>
     <p>page not found</p>
-    <p><a href={"/"}>home</a></p>
+    <p><Link to={"/"}>home</Link></p>
     </>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "inferno-router";
+
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
       <p><span role={"img"} aria-label="under construction">🚧</span> under construction</p>
-      <p><a href={"/platformpro"}>platform pro</a></p>
+      <p><Link to={"/platformpro"}>platform pro</Link></p>
     </>
   )
 }
