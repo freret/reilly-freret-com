b1b9af5cb9f2ec4632f8e7b36a3ff1f19f40b5f5
Reilly Freret <freret@wustl.edu>
2023-03-08 17:56:09 -0500

    added education

--- src/Home.js
+++ src/Home.js
@@ -11,9 +11,10 @@ export default function Home() {
       <h2>Currently</h2>
       <p>software and devops engineer at <a href={"https://www.bgcpartners.com/"} target="_blank">bgc partners</a></p>
       <p>creator of <Link to={"/platformpro"}>platform pro</Link></p>
-      
+
       <h2>Recently</h2>
       <p>co-founder and lead engineer at <a href={"https://www.sobrietyhub.com/"} target="_blank">sobriety hub</a></p>
+      <p>b.s. in computer science from <a href={"https://www.wustl.edu"} target="_blank">wash u in st. louis</a></p>
       <p>software engineer at <a href={"https://www.arch-app.com/"} target="_blank">arch app</a></p>
     </>
   )
