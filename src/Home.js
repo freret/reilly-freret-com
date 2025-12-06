8bf68e77e98e4f09ba3e295c7e0f9761a98ade46
Reilly Freret <freret@wustl.edu>
2025-12-06 14:56:21 -0500

    copy update

--- src/Home.js
+++ src/Home.js
@@ -5,19 +5,19 @@ export default function Home() {
   return (
     <>
       <h1>Reilly Freret</h1>
-      <p>software fan</p>
+      <p>code stylist</p>
       <p><Emoji description={"email"} withCharacter="📬" /> [firstname].[lastname]@proton.me</p>
 
       <h2>Currently</h2>
+      <p>software engineer and architect at <a href={"https://with.co"}>withco</a></p>
+
+      <h2>Recently</h2>
+      <p>co-founder and head of engineering at <a href={"https://www.sobrietyhub.com/"} target="_blank">sobriety hub</a></p>
       <p>software and devops engineer at <a href={"https://www.bgcpartners.com/"} target="_blank">bgc partners</a></p>
       <p>creator/maintainer of <Link to={"/platformpro"}>platform pro</Link></p>
       <p>tinkerer on a <Link to={"/billboard"}>better* version of Tidbyt</Link></p>
-      <p style={{"font-style": "italic", color: "gray"}}>*subjective, I guess</p>
-
-      <h2>Recently</h2>
-      <p>co-founder and lead engineer at <a href={"https://www.sobrietyhub.com/"} target="_blank">sobriety hub</a></p>
       <p>b.s. in computer science from <a href={"https://cse.wustl.edu/index.html"} target="_blank">wash u in st. louis</a></p>
-      <p>software engineer at <a href={"https://www.arch-app.com/"} target="_blank">arch app</a></p>
+      <p style={{"font-style": "italic", color: "gray"}}>*subjective, I guess</p>
     </>
   )
 }
